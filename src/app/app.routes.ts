0911e80a55cf1e3fb4b43376d1614d0ecfaf359a
niassuh1 <galenwaros@gmail.com>
2024-10-30 14:09:13 +0300

    feat: login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import {
+  heroArrowRight,
+  heroChevronLeft,
+  heroEye,
+  heroEyeSlash,
+} from '@ng-icons/heroicons/outline';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    NgIconComponent,
+    RouterLink,
+    InputTextModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    NgIf,
+  ],
+  providers: [
+    provideIcons({ heroArrowRight, heroChevronLeft, heroEye, heroEyeSlash }),
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  passwordVisible = false;
+  togglePasswordVisible() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+}
